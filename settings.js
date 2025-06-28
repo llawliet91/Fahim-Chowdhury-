6808613db1bc45b8c64ebfa8d5598af005e740bc
L Lawliet <159545759+llawliet91@users.noreply.github.com>
2025-06-28 16:15:11 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "FAHIM SIR",
+  botOwner: 'FAHIM CHOWDHURY', // Your name
+  ownerNumber: '+880 1923-353788', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
